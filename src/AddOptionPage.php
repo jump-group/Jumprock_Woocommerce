7b7dd290b12611a60c70a5ad79464d46151a7d5d
Erica Marchi <erica.m@jumpgroup.it>
2023-05-12 10:48:02 +0200

    update add option page file

--- src/AddOptionPage.php
+++ src/AddOptionPage.php
@@ -6,9 +6,17 @@ use function Env\env;
 
 class AddOptionPage
 {
-
     public static function init()
     {
-        // Code here
+        // Add Option Page
+        if (function_exists('acf_add_options_page')) {
+            acf_add_options_page(array(
+                'page_title'    => 'Configurazioni elementi Woocommerce',
+                'menu_title'    => 'Configurazioni elementi Woocommerce',
+                'menu_slug'     => 'woocommerce-elements-config',
+                'capability'    => 'edit_posts',
+                'redirect'      => false
+            ));
+        }
     }
 }
